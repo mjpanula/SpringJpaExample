98f61599cf219b49da95df1f873b277a1d93b604
Panula <Matti.Panula@seamk.fi>
2023-11-20 12:55:06 +0200

    use EntityGraph

--- src/main/java/namesandaddresses/PersonRepository.java
+++ src/main/java/namesandaddresses/PersonRepository.java
@@ -1,7 +1,14 @@
 package namesandaddresses;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PersonRepository extends JpaRepository<Person, Long> {
+    // tämä ratkaisee N+1-kyselyn ongelman, addressit haetaan heti eikä laiskasti
+    // Spring Jpa Eager/Lazy loading
+    @EntityGraph(attributePaths = {"address"})
+    List<Person> findAll();
 
 }
